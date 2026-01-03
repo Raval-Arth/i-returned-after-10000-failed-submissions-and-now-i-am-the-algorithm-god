6e5b68150b96819487637e9aabc7b41c9d0a8950
Arth Raval <connect.arth.raval@gmail.com>
2026-01-03 08:46:07 +0530

    Array: product of array except self

--- Javascript/array.js
+++ Javascript/array.js
@@ -24,8 +24,39 @@ class Soution {
         }
         return maxProfit
     }
+
+    contain_duplicate(nums) {
+        let num_set = new Set(nums)
+
+        console.log('nums.length: ', nums.length);
+        console.log('num_set.length: ', num_set.length);
+        if (nums.length == num_set.length) return false
+        return true
+    }
+
+    product_array_except_self(nums) {
+
+        let result = new Array(nums.length).fill(1);
+
+        let prefix = 1
+        for (let i = 0; i < nums.length; i++) {
+
+            result[i] = prefix
+            prefix = prefix * nums[i]
+        }
+
+        let sufix = 1
+        for (let i = nums.length - 1; i >= 0; i--) {
+            result[i] *= sufix
+            sufix *= nums[i]
+        }
+
+        return result
+    }
 }
 
 let obj = new Soution();
 console.log(obj.two_sum([2, 7, 11, 15], 9));
 console.log(obj.stock_buy_and_sell([7, 1, 5, 3, 6, 4]))
+console.log(obj.contain_duplicate([1, 2, 3, 4]))
+console.log(obj.product_array_except_self([1, 2, 3, 4]))
